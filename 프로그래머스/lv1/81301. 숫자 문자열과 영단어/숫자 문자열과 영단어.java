13c1fb4ce97f2354e501472cb87c61e2ea91a50e
LeeSeungWon97 <110466756+LeeSeungWon97@users.noreply.github.com>
2023-06-09 14:46:14 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 6.56 ms, Memory: 77.3 MB -BaekjoonHub

--- 프로그래머스/lv1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.java
+++ 프로그래머스/lv1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.java
@@ -1,23 +1,12 @@
 class Solution {
      public int solution(String s) {
-    int answer = 0;
+        int answer = 0;
         String[] numCode = {"zero","one","two","three","four","five","six","seven","eight","nine"};
-        while (!checkString(s)){
-            for(int i=0;i<numCode.length;i++){
-                s = s.replaceFirst(numCode[i],i+"");
-            }
+        for(int i=0; i<numCode.length;i++){
+            s=s.replaceAll(numCode[i],i+"");
         }
         answer = Integer.parseInt(s);
         System.out.println(answer);
         return answer;
-    }
-
-    private boolean checkString(String str){
-        try {
-            Long.parseLong(str);
-            return true;
-        }catch (Exception e){
-            return false;
-        }
     }
 }
