f14122d30a820095f806dbb80fd7dbd7db3f3aca
LeeSeungWon97 <110466756+LeeSeungWon97@users.noreply.github.com>
2023-06-14 09:03:25 +0900

    [unrated] Title: 추억 점수, Time: 0.08 ms, Memory: 72.9 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/176963. 추억 점수/추억 점수.java
@@ -0,0 +1,15 @@
+import java.util.Arrays;
+class Solution {
+    public int[] solution(String[] name, int[] yearning, String[][] photo) {
+        int[] answer = new int[photo.length];
+        for(int i=0; i<photo.length; i++){
+            for(int j=0; j<photo[i].length;j++){
+                int idx = Arrays.asList(name).indexOf(photo[i][j]);
+                if(idx != -1){
+                    answer[i] += yearning[idx];
+                }
+            }
+        }
+        return answer;
+    }
+}
