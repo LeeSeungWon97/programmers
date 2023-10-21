880a998b0ff7e473d2a98bb64153e9f6ae44fbf1
LeeSeungWon97 <110466756+LeeSeungWon97@users.noreply.github.com>
2023-10-21 17:47:58 +0900

    [level 2] Title: 올바른 괄호, Time: 16.67 ms, Memory: 53.1 MB -BaekjoonHub

---
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -0,0 +1,18 @@
+import java.util.Stack;
+class Solution {
+    boolean solution(String s) {
+        Stack<Character> stack = new Stack();
+        for (char c : s.toCharArray()) {
+            if (c == '(') {
+                stack.push(c);
+            } else if (c==')') {
+                if (!stack.isEmpty()) {
+                    stack.pop();
+                } else {
+                    return false;
+                }
+            }
+        }
+        return stack.isEmpty();
+    }
+}
