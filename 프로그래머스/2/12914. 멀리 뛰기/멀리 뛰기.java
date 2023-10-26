f6ce0fd6cb70607a06f59d26e48a1364a7046c0c
LeeSeungWon97 <110466756+LeeSeungWon97@users.noreply.github.com>
2023-10-26 17:57:02 +0900

    [level 2] Title: 멀리 뛰기, Time: 0.11 ms, Memory: 79.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.java
@@ -0,0 +1,11 @@
+class Solution {
+    public long solution(int n) {
+        long[] distance = new long[n+2];
+        distance[1] = 1;
+        distance[2] = 2;
+        for (int i = 3; i < n + 1; i++) {
+            distance[i] = (distance[i-2] + distance[i-1]) % 1234567;
+        }
+        return distance[n];
+    }
+}
